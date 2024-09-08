67cd5c4b368723f009ae71a52859f751e16aa61c
maria-skygrid <maria.cosmen@skygrid.co.jp>
2024-09-08 23:08:05 +0900

    feature/add books_by_published_year endpoint

--- fastapienv/books2.py
+++ fastapienv/books2.py
@@ -12,14 +12,16 @@ class Book:
     rating: float
     pages: int
     category: str
+    published_year: int
 
-    def __init__(self, id, title, author, rating, pages, category):
+    def __init__(self, id, title, author, rating, pages, category, published_year):
         self.id = id
         self.title = title
         self.author = author
         self.rating = rating
         self.pages = pages
         self.category = category
+        self.published_year = published_year
 
 # for validation
 class BookRequest(BaseModel):
@@ -29,6 +31,7 @@ class BookRequest(BaseModel):
     rating: float = Field(gt=-1, le=5)
     pages: int = Field(gt=0)
     category: str
+    published_year: int
 
     model_config = {
 	"json_schema_extra": {
@@ -38,6 +41,7 @@ class BookRequest(BaseModel):
             "rating": 3.5,
 			"pages": 100, 
 			"category": "new category", 
+            "published_year": 2020,
 		}
 	}
 }
@@ -45,14 +49,14 @@ class BookRequest(BaseModel):
 
 
 BOOKS = [
-    Book(id=1, title="Murder in the Nile", author="Agatha Christie", rating=3, pages=200, category="mistery"),
-    Book(id=2, title="Murder in the Orient Express", author="Agatha Christie", rating=4, pages=200, category="mistery"),
-    Book(id=3, title="The Dark Forest", author="Liu Cixin", rating=4.5, pages=750, category="scifi"),
-    Book(id=4, title="The Three Body Problem", author="Liu Cixin", rating=3, pages=600, category="scifi"),
-    Book(id=5, title="Pet Cemetery", author="Stephen King", rating=5, pages=500, category="horror"),
-    Book(id=6, title="The Shining", author="Stephen King", rating=3, pages=666, category="horror"),
-    Book(id=7, title="Dune", author="Frank Herbert", rating=3.5, pages=600, category="scifi"),
-    Book(id=8, title="The Thursday Murder Club", author="Richard Osman", rating=4.5, pages=350, category="mistery"),
+    Book(id=1, title="Murder in the Nile", author="Agatha Christie", rating=3, pages=200, category="mistery", published_year=1980),
+    Book(id=2, title="Murder in the Orient Express", author="Agatha Christie", rating=4, pages=200, category="mistery", published_year=1986),
+    Book(id=3, title="The Dark Forest", author="Liu Cixin", rating=4.5, pages=750, category="scifi", published_year=2016),
+    Book(id=4, title="The Three Body Problem", author="Liu Cixin", rating=3, pages=600, category="scifi", published_year=2014),
+    Book(id=5, title="Pet Cemetery", author="Stephen King", rating=5, pages=500, category="horror", published_year=2002),
+    Book(id=6, title="The Shining", author="Stephen King", rating=3, pages=666, category="horror", published_year=1999),
+    Book(id=7, title="Dune", author="Frank Herbert", rating=3.5, pages=600, category="scifi", published_year=1976),
+    Book(id=8, title="The Thursday Murder Club", author="Richard Osman", rating=4.5, pages=350, category="mistery", published_year=2020),
 ]
 
 # - - - - - - -
@@ -65,9 +69,13 @@ def index():
 def show(book_id: int):
     return next((book for book in BOOKS if book.id == book_id), None)
 
-@app.get("/books/")
-def book_by_rating(book_rating: int):
+@app.get("/books/by-rating/{book_rating}")
+def book_by_rating(book_rating: float):
     return [book for book in BOOKS if book.rating == book_rating ]
+
+@app.get("/books/by-published-year/{book_published_year}")
+def books_by_published_year(published_year: int):
+    return [book for book in BOOKS if book.published_year == published_year]
 
 @app.post("/books/create-book")
 def create(book_request: BookRequest):
