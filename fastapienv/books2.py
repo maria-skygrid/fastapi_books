11b4f84d51a3cde7d8736fff00d90065df4840ae
maria-skygrid <maria.cosmen@skygrid.co.jp>
2024-09-08 22:41:49 +0900

    feature/add books_by_rating endpoint

--- fastapienv/books2.py
+++ fastapienv/books2.py
@@ -9,13 +9,15 @@ class Book:
     id: int
     title: str
     author: str
+    rating: float
     pages: int
     category: str
 
-    def __init__(self, id, title, author, pages, category):
+    def __init__(self, id, title, author, rating, pages, category):
         self.id = id
         self.title = title
         self.author = author
+        self.rating = rating
         self.pages = pages
         self.category = category
 
@@ -24,6 +26,7 @@ class BookRequest(BaseModel):
     id: Optional[int] = Field(description="ID is not required on Create", default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
+    rating: float = Field(gt=-1, le=5)
     pages: int = Field(gt=0)
     category: str
 
@@ -32,6 +35,7 @@ class BookRequest(BaseModel):
 		"example": {
 			"title": "New book", 
 			"author": "new author", 
+            "rating": 3.5,
 			"pages": 100, 
 			"category": "new category", 
 		}
@@ -41,14 +45,14 @@ class BookRequest(BaseModel):
 
 
 BOOKS = [
-    Book(1, "Murder in the Nile", "Agatha Christie", 200, "mistery"),
-    Book(2, "Murder in the Orient Express", "Agatha Christie", 200, "mistery"),
-    Book(3, "The Dark Forest", "Liu Cixin", 750, "scifi"),
-    Book(4, "The Three Body Problem", "Liu Cixin", 600, "scifi"),
-    Book(5, "Pet Cemetery", "Stephen King", 500, "horror"),
-    Book(6, "The Shining", "Stephen King", 666, "horror"),
-    Book(7, "Dune", "Frank Herbert", 600, "scifi"),
-    Book(8, "The Thursday Murder Club", "Richard Osman", 350, "mistery"),
+    Book(id=1, title="Murder in the Nile", author="Agatha Christie", rating=3, pages=200, category="mistery"),
+    Book(id=2, title="Murder in the Orient Express", author="Agatha Christie", rating=4, pages=200, category="mistery"),
+    Book(id=3, title="The Dark Forest", author="Liu Cixin", rating=4.5, pages=750, category="scifi"),
+    Book(id=4, title="The Three Body Problem", author="Liu Cixin", rating=3, pages=600, category="scifi"),
+    Book(id=5, title="Pet Cemetery", author="Stephen King", rating=5, pages=500, category="horror"),
+    Book(id=6, title="The Shining", author="Stephen King", rating=3, pages=666, category="horror"),
+    Book(id=7, title="Dune", author="Frank Herbert", rating=3.5, pages=600, category="scifi"),
+    Book(id=8, title="The Thursday Murder Club", author="Richard Osman", rating=4.5, pages=350, category="mistery"),
 ]
 
 # - - - - - - -
@@ -57,16 +61,20 @@ BOOKS = [
 def index():
     return BOOKS
 
-
 @app.get("/books/{book_id}")
 def show(book_id: int):
     return next((book for book in BOOKS if book.id == book_id), None)
+
+@app.get("/books/")
+def book_by_rating(book_rating: int):
+    return [book for book in BOOKS if book.rating == book_rating ]
 
 @app.post("/books/create-book")
 def create(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
     new_book.id = increase_book_id()
     BOOKS.append(new_book)
+
 
 # - - - 関数
 def increase_book_id():
